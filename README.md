3c5d55322ee6f80a2be989afaf4879820577cbae
Taran Pierce <taran.pierce@newfold.com>
2022-06-25 18:55:53 -0500

    master
    pushing up new test build

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # clbt-prod
 
-## Instructions
+## Commands
 - `npm run setup` - setup and install packages for site
+- `npm run dev` - run site locally
+- `npm run start` - start up the server locally
+- `npm run build:clean` - cleans current build to remove it
+- `npm run build:create` - creates production build
+- `npm run build:move` - moves build from app/out
+
+## Instructions
+- Clean build
+- Create new build
+- Move it
+- Create tag
---
+++ clean.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# list of files to be replaced
+declare -a fileList=("index.html" 
+                "robots.txt"
+                "sitemap.xml"
+                "manifest.json"
+                "google6054a454639042bb.html"
+                "_next/"
+                "404/"
+                "404.html/"
+                "about/"
+                "contact/"
+                "css/"
+                "directions/"
+                "images/"
+                "photo-gallery/"
+                "things-to-do/"
+                )
+
+## loop through and delete them
+for i in "${fileList[@]}"
+
+do
+   echo "Deleting $i...."
+   eval "rm -r $i"
+done
