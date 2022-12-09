bed69b89639e7385c1853eb823e4e8c8e31e3ec3
Taran Pierce <taran.tpdesign@gmail.com>
2022-12-08 17:26:26 -0700

    master
    updated production build
    
    - adds back contact form

---
+++ _next/static/7a1AQDaMmXOz8qqYoGhQW/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fphoto-gallery","\u002Fthings-to-do","\u002Fabout","\u002Fdirections","\u002Fcontact"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/xab1o51rciIll7SGFE2ua/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fphoto-gallery","\u002F","\u002Fabout","\u002Fthings-to-do","\u002Fdirections","\u002Fcontact"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
